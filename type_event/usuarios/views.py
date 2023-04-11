b9c9e44e09c919a0c36277d9d7c7d99604a5e98d
rick <rickyamas@gmail.com>
2023-04-11 14:08:47 -0300

    :bulb: Se rodar e ir no endereço /usuarios/cadastro/ temos  uma aplicação

--- type_event/usuarios/views.py
+++ type_event/usuarios/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-#    if request.method == "GET":
-#       return render(request, 'cadastro.html')
+#   if request.method == "GET":
+    return render(request, 'cadastro.html')
