da57ac38e189049d6bff8a54088dfa577ac74f1a
rick <rickyamas@gmail.com>
2023-04-12 02:39:35 -0300

    Verifica se a senha sao igual caso contrario permanece na mesma pagiana

--- type_event/usuarios/views.py
+++ type_event/usuarios/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
-# Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        return HttpResponse ('testando')
+
+        # vai pegar os dados que esta la no cadasro 
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        # verifica a senha, caso não for igual voltar no cadastro 
+        if not (senha == confirmar_senha):    
+            return redirect('/usuarios/cadastro')
+#           return redirect(reverse('cadastro'))
+        
+        return HttpResponse ('teste')
+        # Vamos criar uma variavel para podemo enviar
+ #       user = User.objects.filter(username=username)
+
+#        if user.exists():
+#            return redirect(reverse('cadastro'))   
+        
+ #       user = User.objects.create_user(username=username, email=email, password=senha)
+  #      user.save()
+   #     return redirect(reverse('login'))
