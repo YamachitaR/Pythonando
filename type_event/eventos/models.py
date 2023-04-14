48e863b5e13de82efe12f318b0893047c8e893e5
rick <rickyamas@gmail.com>
2023-04-14 12:18:36 -0300

    criamos um banco de dado para certificado

--- type_event/eventos/models.py
+++ type_event/eventos/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from django.contrib.auth.models import User
 
 # Estamos criando o nosso banco de dados manual, acredito seja por causa da complexibilidade
@@ -32,8 +31,14 @@ class Evento(models.Model):
     def __str__(self):
         return self.nome
     
-    # Agora que terminamos o django  vai facilitar fazendo as paradas 
+    # Agora que terminamos o django  vai facilitar fazendo as paradas tb para atualizar
     # python3 manage.py makemigrations
     # python3 manage.py migrate
     # Os dois comando de cima serve para criar ou atualizar o banco de dados 
 
+
+# Criar banco de dados para o certificado
+class Certificado(models.Model):
+    certificado = models.ImageField(upload_to="certificados")  # certificado vai ser salvo
+    participante = models.ForeignKey(User, on_delete=models.DO_NOTHING) # relaçao do dado, um para n, usuario para varios eventos
+    evento = models.ForeignKey(Evento, on_delete=models.DO_NOTHING) # analogo, so que um evento para varios usuarios
