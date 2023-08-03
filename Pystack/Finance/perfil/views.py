461be7bda28ed4ada2acc6397564b149fc3c2310
rick <rickyamas@gmail.com>
2023-08-03 03:40:35 -0300

    organizando: foi organizado as pastas

---
+++ Pystack/Finance/perfil/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Conta, Categoria
+from django.contrib import messages
+from django.contrib.messages import constants
+from .utils import calcula_total,calcula_equilibrio_financeiro
+from extrato.models import Valores
+from datetime import datetime
+
+def home(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    entradas = valores.filter(tipo='E')
+    saidas = valores.filter(tipo='S')
+
+    total_entradas = calcula_total(entradas, 'valor')
+    total_saidas = calcula_total(saidas, 'valor')
+
+    contas = Conta.objects.all()
+    saldo_total = calcula_total(contas, 'valor')
+
+    percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro()
+
+    print(percentual_gastos_essenciais, percentual_gastos_nao_essenciais)
+    return render(request, 'home.html',
+                   {'contas': contas,
+                    'saldo_total': saldo_total, 
+                     'total_entradas': total_entradas, 
+                     'total_saidas': total_saidas,
+                     'percentual_gastos_essenciais': int(percentual_gastos_essenciais),
+                     'percentual_gastos_nao_essenciais':int(percentual_gastos_nao_essenciais)
+                     })
+
+
+
+def gerenciar(request):
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+   
+    total_contas = calcula_total(contas, 'valor')
+
+    print(total_contas)
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias})
+
+
+
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    # falta fazer vaidação de outras variáveis
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos!')
+        return redirect('/perfil/gerenciar/')
+    
+    conta = Conta(
+        apelido = apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+
+    conta.save()
+    messages.add_message(request, constants.SUCCESS, 'Cadastro com Sucesso!')
+
+    return redirect('/perfil/gerenciar/')
+
+def deletar_banco(request, id):
+    conta = Conta.objects.get(id=id)
+    conta.delete()
+    
+    messages.add_message(request, constants.SUCCESS, 'Conta removida com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    # fazer validações 
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+
+    categoria.essencial = not categoria.essencial
+
+    categoria.save()
+
+    return redirect('/perfil/gerenciar/')
+
+
+def dashboard(request):
+    dados = {}
+    categorias = Categoria.objects.all()
+
+    for categoria in categorias:
+        total = 0
+        valores = Valores.objects.filter(categoria=categoria)
+        for v in valores:
+            total += v.valor       
+        dados[categoria.categoria] = total
+        
+        # dados[categoria.categoria] = Valores.objects.filter(categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
---
+++ README.md
@@ -0,0 +1,15 @@
+# Pythonando
+
+Pythonando é um curso de Python e Django, administrado pelo professor Caio Sampaio  
+
+## Pystack Week
+
+Pystack Week é um evento onde o professor Caio ensina desenvolver uma aplicação com Python e Django.
+
+Até o momento eu desenvolvi junto com professor os seguintes projetos: 
+
+ 
+- Type Event:  um projeto de gerenciamento de evento;
+- Finance:  um projeto de gerenciamento  de finança.
+
+
