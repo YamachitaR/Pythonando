6dab050afce16d7dbe3908b014287b6bf740fd64
rick <rickyamas@gmail.com>
2023-10-07 20:08:38 -0300

    vitalab: cadastro finalizado

---
+++ Pystack/vitalab/usuarios/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
+# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não são iguais')
+            return redirect('/usuarios/cadastro')
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha tem que ter mais de 6 digitos')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            # To do:Username deve ser único!
+            # esse User é do proprio do Django que ajudar a validar os dados
+            user = User.objects.create_user(
+                first_name=primeiro_nome,
+                last_name=ultimo_nome,
+                username=username,
+                email=email,
+                password=senha,
+                )
+            messages.add_message(request, constants.SUCCESS, 'Cadastro com Sucesso!!!!!!!!!!')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro ao cadastrar')
+            return redirect('/usuarios/cadastro')
+        
+
+        return redirect('/usuarios/cadastro')
