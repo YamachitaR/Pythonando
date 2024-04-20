476ed974f9dc37c4da753657c0f6a1bef792fd5c
Ricardo <rickyamas@gmail.com>
2024-04-20 16:54:32 -0300

    create mensagem in cadastro

--- Pystack/healing/usuarios/views.py
+++ Pystack/healing/usuarios/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+
+from django.contrib import messages
+
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+
+        # vamos utilizar p/ verificar se exite usuario já cadastrado 
+        users = User.objects.filter(username=username) 
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Esse usuario já existe')
+            return redirect('/usuarios/cadastro')
+
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'A senha e o confirmar senha devem ser iguais')
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha deve possuir pelo menos 6 caracteres')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            # Criando usuario  
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            print('Erro 4')
+            return redirect('/usuarios/cadastro')
